f4867814b348a6d08700af1acdce49edd840abc8
Sergey Nesterov <sergey.nesterov@lykke.com>
2020-04-06 16:14:26 +0300

    Fixed AutofacExtension

--- Lykke.Service.LykkeService/client/Lykke.Service.LykkeService.Client/AutofacExtension.cs
+++ Lykke.Service.LykkeService/client/Lykke.Service.LykkeService.Client/AutofacExtension.cs
@@ -1,8 +1,8 @@
-﻿using Autofac;
+﻿using System;
+using Autofac;
 using JetBrains.Annotations;
 using Lykke.HttpClientGenerator;
 using Lykke.HttpClientGenerator.Infrastructure;
-using System;
 
 namespace Lykke.Service.LykkeService.Client
 {
@@ -30,7 +30,7 @@ namespace Lykke.Service.LykkeService.Client
             if (string.IsNullOrWhiteSpace(settings.ServiceUrl))
                 throw new ArgumentException("Value cannot be null or whitespace.", nameof(LykkeServiceServiceClientSettings.ServiceUrl));
 
-            var clientBuilder = HttpClientGenerator.HttpClientGenerator.BuildForUrl(settings.ServiceUrl)
+            var clientBuilder = HttpClientGenerator.BuildForUrl(settings.ServiceUrl)
                 .WithAdditionalCallsWrapper(new ExceptionHandlerCallsWrapper());
 
             clientBuilder = builderConfigure?.Invoke(clientBuilder) ?? clientBuilder.WithoutRetries();
